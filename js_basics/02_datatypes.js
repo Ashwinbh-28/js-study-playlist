59e389d1add36ecc1b0f5ca640910ad2e8d4dfb1
Ashwin Bhanage <153538156+Ashwinbh-28@users.noreply.github.com>
2024-06-23 03:32:04 +0000

    datatypes

---
+++ js_basics/02_datatypes.js
@@ -0,0 +1,29 @@
+/* 
+Primitive Datatypes in JS
+number => number declared 
+string => formation of the array prefer to use ""
+null => "null" does not mean zero it  is that value not available
+undefined => empty or not declared value
+symbol => used for uniqueness in react JS 
+boolean => simplify used for true/false values 
+*/
+
+
+/*
+Study Materials of JS :  
+https://tc39.es/ecma262/2023/multipage/ - Study Material for JS types
+https://developer.mozilla.org/en-US/docs/Web/JavaScript - USed for JS  in this playlist 
+*/
+
+
+let age = 23
+let name = "ashwin";
+let state;
+
+
+console.log(typeof age);
+console.log(typeof name); 
+console.log(typeof null); // the Datatype of null is "Object" 
+console.log(typeof undefined); ///for the undefined is "undefined"
+
+
