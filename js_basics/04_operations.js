bcd674b5b239acc101cfed73ca55b294de49ebdd
Ashwin Bhanage <153538156+Ashwinbh-28@users.noreply.github.com>
2024-06-25 08:17:17 +0000

    operation

---
+++ js_basics/04_operations.js
@@ -0,0 +1,12 @@
+/*
+note that: For the increments of the operators like "++x", the value
+returns the incremented value for and returns the value for the both as same
+
+But for the "x++" value the increment operator is returned with the value of incremented and then
+return the input values  
+*/
+
+let x = 3
+let y = x++
+
+console.log(a={x}, b={y});
